ba487fd43e8c7f1bc84e798fb6832e0e9bf61f98
G(x) <2050230094@qq.com>
2026-04-16 04:18:40 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,42 @@
-await window.Clerk.load();
-const token = await window.Clerk.session.getToken();
-const response = await fetch('https://api.gxweb.top/get-list', {
-    method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${token}`,
-    },
-});
-if (response.redirected) {
-    window.location.href = response.url;
-} else {
-    const files = await response.json();
-    console.log(files);
+const _ = window._;
+const { createApp, ref, computed } = window.Vue;
+
+async function getFileList() {
+    await window.Clerk.load();
+    const token = await window.Clerk.session.getToken();
+    const response = await fetch('https://api.gxweb.top/get-list', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+    });
+    if (response.redirected) {
+        window.location.href = response.url;
+        return [];
+    } else {
+        const filesList = await response.json();
+        return filesList;
+    }
 }
+
+async function bootstrap() {
+    const filesList = await getFileList();
+    const app = createApp ({
+        setup() {
+            const rawData = ref(filesList);
+            const theFolders = computed(() => {
+                return _.chain(rawData.value)
+                    .filter(item => item.size === '0')
+                    .filter(item => /^[^/]+\/$/.test(item.key))
+                    .map(item => ({
+                        name: item.key.slice(0, -1),
+                        key: item.key,
+                    }))
+                    .value();
+            });
+            return {theFolders};
+        }
+    })
+}
+
+bootstrap();
